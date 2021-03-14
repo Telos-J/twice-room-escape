7f944eb571169ad8d29a7af367bf07c47a7ade75
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-03-14 09:11:19 +0900

    bubble animation

---
+++ assets/js/bubbleAnimation.js
@@ -0,0 +1,15 @@
+//register the plugin (just once)
+gsap.registerPlugin(MotionPathPlugin);
+
+document.querySelector('#bubble-tea').addEventListener('click', () => {
+    gsap.to("#bubble1", {
+    duration: 1, 
+    ease: CustomEase.create("custom", "M0,0 C0.196,0.322 0.348,0.463 0.54,0.546 0.768,0.644 0.838,0.812 1,1 "),
+    motionPath:{
+        path: "#bubble-path",
+        align: "#bubble-path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5]
+    }
+    });
+})
