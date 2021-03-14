1c830c0dbcc1196dac324387d3c3b84fa4ba45f0
ju0910 <leejooeun0910@gmail.com>
2021-03-14 11:01:39 +0900

    added question

--- assets/js/bubbleAnimation.js
+++ assets/js/bubbleAnimation.js
@@ -1,7 +1,9 @@
 //register the plugin (just once)
 gsap.registerPlugin(MotionPathPlugin);
 
+let clickable = true;
 document.querySelector('#bubble-tea').addEventListener('click', () => {
+    if (clickable)
     gsap.to("#bubble1", {
     duration: 1, 
     ease: CustomEase.create("custom", "M0,0 C0.102,0.2 0.364,0.406 0.5,0.5 0.639,0.596 0.9,0.8 1,1"),
@@ -16,4 +18,6 @@ document.querySelector('#bubble-tea').addEventListener('click', () => {
         document.querySelector('#bubble1').style.display = 'none';
     }
     });
+
+    clickable = false;
 })
