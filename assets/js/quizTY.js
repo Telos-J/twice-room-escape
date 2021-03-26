7c5a3a9b16ec764f3bde80bd30bf785b2f78c67a
ju0910 <leejooeun0910@gmail.com>
2021-03-26 18:42:47 +0900

    deleted bubble animation

--- assets/js/quizTY.js
+++ assets/js/quizTY.js
@@ -1,9 +1,12 @@
 const answers = Array.from (document.querySelectorAll('.tzuyuChoice-text'))
+const quiz = document.querySelector('quiz');
 
 for (let answer of answers) {
     answer.addEventListener('click',()=> {
-        if (answer.innerHTML === '1999.06.14.')
+        if (answer.innerHTML === '1999.06.14.') {
             console.log('You are correct!🎉')
+            quiz.classList.remove('show')
+        }
         else
             console.log('You are wrong, try again...😭')
     })
