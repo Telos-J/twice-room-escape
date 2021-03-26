25e75796b09f8cd7a1233102c20076fc69df47de
ju0910 <leejooeun0910@gmail.com>
2021-03-26 18:24:33 +0900

    tzuyu

--- assets/js/quizTY.js
+++ assets/js/quizTY.js
@@ -1,8 +1,8 @@
-const answers = Array.from (document.querySelectorAll('.nayeonChoice-text'))
+const answers = Array.from (document.querySelectorAll('.tzuyuChoice-text'))
 
 for (let answer of answers) {
     answer.addEventListener('click',()=> {
-        if (answer.innerHTML === 'Khaki')
+        if (answer.innerHTML === '1999.06.14.')
             console.log('You are correct!🎉')
         else
             console.log('You are wrong, try again...😭')
