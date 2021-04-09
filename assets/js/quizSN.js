cf7cdbf529ed62301a6a660af498da88cd48a027
ju0910 <leejooeun0910@gmail.com>
2021-04-09 18:53:23 +0900

    changed quiz questions

--- assets/js/quizSN.js
+++ assets/js/quizSN.js
@@ -1,4 +1,4 @@
 const quiz1 = new Quiz("What are bacteria in the ecosystem?", ["Producers", "Primary consumers", "Secondary consumers", "Decomposers"], 3);
 const quiz2 = new Quiz("When is World environment day celebrated every year?", ["August 12", "June 5", "September 8", "July 10"], 1);
-const quiz3 = new Quiz("When is Sana's birthday?", ["1996.12.29.", "1996.12.07.", "1996.12.18.", "1996.12.25."], 0);
-const quiz4 = new Quiz("Which language is Sana fluent at apart from Japanese?", ["Chinese", "French", "English", "Korean"], 3);
+const quiz3 = new Quiz("What is the atmospheric layer closest to the ground?", ["Troposphere", "Mesosphere", "Thermosphere", "Stratosphere"], 0);
+const quiz4 = new Quiz("What is the most abundant gas on the earth's atmosphere?", ["Oxygen", "Carbon", "Nitrogen", "Hydrogen"], 2);
